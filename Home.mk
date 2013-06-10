ba59b2aeb717cc080faefbb99b5e4b047cd7b95a
Chad Versace <chad@kiwitree.net>
2013-06-10 10:32:10 -0700

    make: Traverse into subdirs of /opt

--- Home.mk
+++ Home.mk
@@ -7,6 +7,11 @@ TARGETS := \
 .PHONY: install
 install: $(TARGET_DIR) $(addprefix $(TARGET_DIR)/,$(TARGETS))
 	@make -C .config -f Home.mk install
+	@for i in opt/*; do \
+	    if [ -r $i/Home.mk ]; then \
+		make -C $i -f Home.mk install; \
+	    fi; \
+	done
 
 $(TARGET_DIR)/%: %
 	install -t $(TARGET_DIR) $^
