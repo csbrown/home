23fc388788748333c2b3f24278a333badbf493b5
Chad Versace <chad@kiwitree.net>
2013-06-12 14:51:32 -0700

    repo: Import Google's repo tool
    Add submodule a ~/opt/git-repo of my fork of git-repo. Link the main
    script to ~/bin/repo.

--- opt/Home.mk
+++ opt/Home.mk
@@ -1,11 +1,17 @@
 TARGET_DIR := ~/opt
 
+SYNC_SUBDIRS := \
+    git-repo \
+    taro
+
 .PHONY: install
 install: $(TARGET_DIR)
-	@rsync -va --delete \
-	    -f '- .git' \
-	    -f '- .gitignore' \
-	    taro/ $(TARGET_DIR)/taro/
+	@for i in $(SYNC_SUBDIRS); do \
+	    rsync -va --delete \
+		-f '- .git' \
+		-f '- .gitignore' \
+		$$i/ $(TARGET_DIR)/$$i/; \
+	done
 
 $(TARGET_DIR)/%: %
 	install -t $(TARGET_DIR) $^
