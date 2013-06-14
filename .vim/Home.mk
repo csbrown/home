dc48be29c72e063c4bb3ebbe3c5798b297eb6cde
Chad Versace <chad@kiwitree.net>
2013-06-14 09:04:26 -0700

    vim: Generate helptags during install

--- .vim/Home.mk
+++ .vim/Home.mk
@@ -6,7 +6,10 @@ install:
 	    -f '- .git' \
 	    -f '- .gitignore' \
 	    -f '- Home.mk' \
+	    -f '- tags' \
 	    ./ $(TARGET_DIR)/
+	@echo "vim: Generate helptags"
+	@vim -c "call pathogen#helptags()" -c quit
 
 $(TARGET_DIR):
 	install -d $(TARGET_DIR)
