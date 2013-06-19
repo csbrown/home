22892e67ac662875de224eff600a221c6a9b0da7
Chad Versace <chad@kiwitree.net>
2013-06-18 20:00:14 -0700

    applications: Change install method 'files' -> 'rsync'
    It's safe to use rsync here as long as we don't use --delete. Otherwise,
    the target directory gets clobbered, which is bad for
    ~/.local/share/applications.

--- .local/share/applications/Home.mk
+++ .local/share/applications/Home.mk
@@ -2,10 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(HOME_CLEAR_VARS)
 
 LOCAL_INSTALL_DIR := ~/.local/share/applications
-LOCAL_INSTALL_METHOD := files
-LOCAL_FILES := \
-    gtk-redshift.desktop \
-    null-component.desktop \
-    null-provider.desktop
-
+LOCAL_INSTALL_METHOD := rsync
+LOCAL_RSYNC_NO_DELETE := true
 include $(HOME_INSTALL_MODULE)
