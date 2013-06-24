f29f67a11d9bc9a6bba20416c6a4694735eb81e1
Chad Versace <chad@kiwitree.net>
2013-06-23 17:35:55 -0700

    gnome-session: Permit per-box selection of gnome session
    Some of my boxes want a dummy gnome session because they use systemd to
    mange the user session. Other boxes want a vanilla gnome session.
    
    However, the makefiles ignored this pre-box setup.
    
    To permit each box to choose its own gnome session, remove
        $git_top/.config/gnome-session/session/gnome.session
        $git_top/.config/gnome-session/session/gnome-fallback.session
    and fix Home.mk to not delete the corresponding real files in $HOME.

--- .config/gnome-session/sessions/Home.mk
+++ .config/gnome-session/sessions/Home.mk
@@ -3,4 +3,7 @@ include $(HOME_CLEAR_VARS)
 
 LOCAL_INSTALL_DIR := ~/.config/gnome-session/sessions
 LOCAL_INSTALL_METHOD := rsync
+LOCAL_RSYNC_EXCLUDES := \
+    gnome.session \
+    gnome-fallback.session
 include $(HOME_INSTALL_MODULE)
