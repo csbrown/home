5ac3bf2b0f3bef9095fd23c6b6b5ac834b07838a
Chad Versace <chad@kiwitree.net>
2013-11-10 09:35:00 -0800

    profile: Fix addition of ~/bin to PATH
    kw_path_prepend must be called with exactly one director at a time, not
    with a colon-separated list.

--- .profile.d/90-home-bin.sh
+++ .profile.d/90-home-bin.sh
@@ -1,2 +1,3 @@
-# Give these paths priority.
-export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+# Prepend these paths last to give them priority.
+kw_path_prepend "$HOME/bin"
+kw_path_prepend "$HOME/.local/bin"
