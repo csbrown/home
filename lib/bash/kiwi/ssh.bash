24b539ef898fdac8ae62bcb484109518842e2d98
Chad Versace <chad@kiwitree.net>
2013-11-26 07:38:06 -0800

    ssh: Rename vars from SSH namespace into kw_ssh namespace

--- lib/bash/kiwi/ssh.bash
+++ lib/bash/kiwi/ssh.bash
@@ -1,13 +1,13 @@
-export SSH_AGENT_ENV="$HOME/.ssh/ssh-agent.env"
-export SSH_AGENT_LOCK="$HOME/.ssh/ssh-agent.lock"
+kw_ssh_agent_env_file="$HOME/.ssh/ssh-agent.env"
+kw_ssh_agent_lock="$HOME/.ssh/ssh-agent.lock"
 
 function kw_ssh_start_agent() {
     if ! [[ -d "$HOME/.ssh" ]]
     then return 1
     fi
 
-    if ! flock -n "$SSH_AGENT_LOCK" \
-            sh -c "ssh-agent > '$SSH_AGENT_ENV'"
+    if ! flock -n "$kw_ssh_agent_lock" \
+            sh -c "ssh-agent > '$kw_ssh_agent_env_file'"
     then return 1
     fi
 
@@ -15,7 +15,7 @@ function kw_ssh_start_agent() {
 }
 
 function kw_ssh_connect_to_agent() {
-    source "$SSH_AGENT_ENV" >&/dev/null || return 1
+    source "$kw_ssh_agent_env_file" >&/dev/null || return 1
     kill -0 "$SSH_AGENT_PID" >&/dev/null || return 1
     echo "ssh: Connected to agent $SSH_AUTH_SOCK:$SSH_AGENT_PID"
 }
