0d686f3b392b22a7fc06d160896306899aadc323
Chad Versace <chad@kiwitree.net>
2013-12-27 08:31:03 -0800

    profile: Refactor kw_path_prepend(), kw_path_append()
    Define two new functions, kw_dirlist_prepend() and kw_dirlist_append().
    Redefined kw_path_prepend() to wrap kw_dirlist_prepend(), ditto for
    append.

--- .profile.d/10-profile-functions.sh
+++ .profile.d/10-profile-functions.sh
@@ -1,11 +1,25 @@
-function kw_path_prepend() {
-    if [[ -d "$1" ]]; then
-        export PATH="$1:$PATH"
+function kw_dirlist_prepend() {
+    local list_name="$1"
+    local dir="$2"
+
+    if [[ -d "${dir}" ]]; then
+        export "${list_name}"="${dir}:${!list_name}"
     fi
 }
 
-function kw_path_append() {
-    if [[ -d "$1" ]]; then
-        export PATH="$PATH:$1"
+function kw_dirlist_append() {
+    local list_name="$1"
+    local dir="$2"
+
+    if [[ -d "${dir}" ]]; then
+        export "${list_name}"="${!list_name}:${dir}"
     fi
+}
+
+function kw_path_prepend() {
+    kw_dirlist_prepend PATH "$1"
+}
+
+function kw_path_append() {
+    kw_dirlist_append PATH "$1"
 }
