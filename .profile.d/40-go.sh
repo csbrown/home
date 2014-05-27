700d8d3962202a3bfc8d77132b9debf85a541bcf
Chad Versace <chad.versace@linux.intel.com>
2014-05-26 17:11:11 -0700

    profile/go: Don't set kw_goroot
    It's only user was an old vimrc.

--- .profile.d/40-go.sh
+++ .profile.d/40-go.sh
@@ -2,10 +2,5 @@ if ! which go >&/dev/null; then
     return
 fi
 
-# Other config files should use $kw_goroot to find the GOROOT.  We don't set
-# GOROOT itself because that's generally a bad idea unless go is installed to
-# a custom location or if one is developing go itself.
-export kw_goroot=$(eval $(go env) && echo $GOROOT)
-
 export GOPATH="$HOME/go"
 kw_path_prepend "$GOPATH/bin"
